feb6ef535e14eb126001751b3fd0d0a4c045ef5f
Arittro7 <arittrowpdev@gmail.com>
2025-01-30 10:06:28 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wv413.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let db;
+
+client.connect().then(() => {
+  db = client.db("addressBook");
+  console.log("Smart Book is connected to MongoDB");
+}).catch(err => console.error("MongoDB connection error:", err));
+
 
 
 
